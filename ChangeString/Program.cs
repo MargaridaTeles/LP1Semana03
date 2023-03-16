f466054f260a02aa9a30244339b87ed97a04a374
Maggiee <telesmaegarida@gmail.com>
2023-03-16 23:00:36 +0000

    Adicionei código à solução

---
+++ ChangeString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira uma frase: ");
+            string frase = Console.ReadLine();
+            Console.Write("Insira um caracter: ");
+            char carater = char.Parse(Console.ReadLine());
+
+            foreach(char c in frase)
+            {
+                if(c!=carater)
+                {
+                    Console.Write(c);
+                }
+            }
+
+        }
+    }
+}
