097f36cf4ce442391a63bf3947123c7ff28199d4
MargaridaTeles <telesmaegarida@gmail.com>
2023-03-17 14:20:41 +0000

    switch Perks

--- PlayerPerks/Program.cs
+++ PlayerPerks/Program.cs
@@ -6,7 +6,31 @@ namespace PlayerPerks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Perks myperks = 0;
+            foreach(char c in args[0])
+            {
+                if(c=='w')
+                {
+                    myperks ^= Perks.WaterBreathing;
+                }
+                else if(c=="a")
+                {
+                    myperks ^= Perks.AutoHeal;
+                }
+                else if(c=="s")
+                {
+                    myperks ^= Perks.Stealth;
+                }
+                else if(c=="d")
+                {
+                    myperks ^= Perks.DoubleJump;
+                }
+                else
+                {
+                    Console.WriteLine("Unknow perk!");
+                }
+            }
         }
     }
 }
