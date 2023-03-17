a46604e40157641c265dc0ef06036e698906e5b9
MargaridaTeles <telesmaegarida@gmail.com>
2023-03-17 17:18:01 +0000

    Conquistas dos Players

--- PlayerAchievements/Program.cs
+++ PlayerAchievements/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AchievementsPlayer;
 
 namespace PlayerAchievements
 {
@@ -6,7 +7,56 @@ namespace PlayerAchievements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Quantos players existem? ");
+            int n = int.Parse(Console.ReadLine());
+
+            achievements achievements;
+            achievements[] players = new achievements[n];
+
+            string questao;
+
+            for (int i = 0; i < n; i++)
+            {
+                achievements = 0;
+                Console.WriteLine($"Conquistas por jogadores {i + 1}: ");
+
+                Console.WriteLine("O player derrotou o boss? (S/N)");
+                questao = Console.ReadLine().ToUpper();
+                if(questao == "S")
+                {
+                    achievements ^= achievements.DefeatOptionalBoss;
+                }
+                Console.WriteLine("O player achou o nivel escondido? (S/N)");
+                questao = Console.ReadLine().ToUpper();
+                if(questao == "S")
+                {
+                    achievements ^= achievements.FindHiddenLevel;
+                }
+                Console.WriteLine("O player acabou o jogo? (S/N)");
+                questao = Console.ReadLine().ToUpper();
+                if(questao == "S")
+                {
+                    achievements ^= achievements.FinishGame;
+                }
+                players[i] = achievements;
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Player {i + 1} achievements: ");
+                if((int)players[i] > 0)
+                {
+                    Console.WriteLine(players[i]);
+                    if((int)players[i]==7)
+                    {
+                        Console.WriteLine("Completionist!");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+
         }
     }
 }
