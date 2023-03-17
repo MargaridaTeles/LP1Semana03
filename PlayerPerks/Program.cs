f08b47b50a8ebcd50e01be6c36976478dc488bc5
MargaridaTeles <telesmaegarida@gmail.com>
2023-03-17 14:54:50 +0000

    Silent jumper! / Not gonna make it!

--- PlayerPerks/Program.cs
+++ PlayerPerks/Program.cs
@@ -6,23 +6,22 @@ namespace PlayerPerks
     {
         static void Main(string[] args)
         {
-
             Perks myperks = 0;
             foreach(char c in args[0])
             {
-                if(c=='w')
+                if(c == 'w')
                 {
                     myperks ^= Perks.WaterBreathing;
                 }
-                else if(c=="a")
+                else if(c == 'a')
                 {
                     myperks ^= Perks.AutoHeal;
                 }
-                else if(c=="s")
+                else if(c == 's')
                 {
                     myperks ^= Perks.Stealth;
                 }
-                else if(c=="d")
+                else if(c == 'd')
                 {
                     myperks ^= Perks.DoubleJump;
                 }
@@ -30,6 +29,22 @@ namespace PlayerPerks
                 {
                     Console.WriteLine("Unknow perk!");
                 }
+            }
+            if(myperks == 0)
+            {
+                Console.WriteLine("No perks at all!");
+            }
+            else
+            {
+                Console.WriteLine($"Perks disponíveis: {myperks}");
+            }
+            if((int)(myperks & (Perks.Stealth | Perks.DoubleJump)) == 10)
+            {
+                Console.WriteLine("Silent jumper!");
+            }
+            if((int)(myperks & (Perks.AutoHeal)) == 0)
+            {
+                Console.WriteLine("Not gonna make it!");
             }
         }
     }
