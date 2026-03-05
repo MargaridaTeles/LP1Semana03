66b552f23b3d4adee96cb890b13f863555906f7e
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-05 09:53:18 +0000

    Update Program.cs for user insert a string, a char and change the string with new char

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,25 @@ namespace ChangeString
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            Console.Write("String: ");
+            string answer = Console.ReadLine();
+
+            
+            Console.Write("Caráter: ");
+            char caracter = char.Parse(Console.ReadLine());
+
+            char x = 'x';
+            string answerUpdate = "";
+
+            foreach (char s in answer)
+            {
+                if (s == caracter)
+                    answerUpdate += x;
+                else
+                    answerUpdate += s;
+            }
+            
+            Console.WriteLine($"Será impresso: {answerUpdate}");
         }
     }
 }
