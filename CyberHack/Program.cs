ef7547db9775830a6abec4c739b60a93344c4a5b
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-05 16:41:05 +0000

    Update Program.cs with conditions

--- CyberHack/Program.cs
+++ CyberHack/Program.cs
@@ -23,6 +23,42 @@ namespace CyberHack
             string zeroDay = HackTool.ZeroDay.ToString();
             string aIOverride = HackTool.AIOverride.ToString();
             string quantumExploit = HackTool.QuantumExploit.ToString();
+
+            if (systemType == corporateServer)
+            {
+                if ((hackTools == phishing || hackTools == backdoor) && (hackTools == bruteForce || hackTools == zeroDay))
+                    Console.WriteLine("System Hacked");
+                else
+                    Console.WriteLine("System Secure");
+            }
+
+            if (systemType == bankDatabase)
+            {
+                if ((hackTools == zeroDay && hackTools == backdoor) || (hackTools == quantumExploit && hackTools == aIOverride))
+                    Console.WriteLine("System Hacked");
+                else
+                    Console.WriteLine("System Secure");
+            }
+
+            if (systemType == smartCityCore)
+            {
+                if ((hackTools == aIOverride && hackTools == backdoor) || (hackTools == zeroDay && hackTools == bruteForce) ||
+                     (hackTools == quantumExploit && hackTools == phishing))
+                    Console.WriteLine("System Hacked");
+                else
+                    Console.WriteLine("System Secure");
+            }
+
+            if (systemType == militaryAI)
+            {
+                if ((hackTools == zeroDay && hackTools == aIOverride) && (hackTools == backdoor || hackTools == bruteForce) &&
+                     (hackTools == phishing || hackTools == quantumExploit))
+                    Console.WriteLine("System Hacked");
+                else
+                    Console.WriteLine("System Secure");
+            }
+
+            Console.WriteLine(hackTools);
         }
     }
 }
