baf69d3eae924a0c96f14b58328c73e04124269f
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-05 16:24:55 +0000

    add atribute for hacktool and systemtype

--- CyberHack/Program.cs
+++ CyberHack/Program.cs
@@ -6,7 +6,23 @@ namespace CyberHack
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            /*HackTool classTool;
+            SystemType classSystem;*/
+
+            string systemType = Console.ReadLine();
+            string hackTools = Console.ReadLine();
+            
+            string corporateServer = SystemType.CorporateServer.ToString();
+            string bankDatabase = SystemType.BankDatabase.ToString();
+            string smartCityCore = SystemType.SmartCityCore.ToString();
+            string militaryAI = SystemType.MilitaryAI.ToString();
+            
+            string bruteForce = HackTool.BruteForce.ToString();
+            string phishing = HackTool.Phishing.ToString();
+            string backdoor = HackTool.Backdoor.ToString();
+            string zeroDay = HackTool.ZeroDay.ToString();
+            string aIOverride = HackTool.AIOverride.ToString();
+            string quantumExploit = HackTool.QuantumExploit.ToString();
         }
     }
 }
