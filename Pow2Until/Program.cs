bbde8857cce3fae9a82b79945ee96291fb162d02
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-11 11:14:14 +0000

    Add new project Pow2Until

---
+++ Pow2Until/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pow2Until
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
