64d95cb3632f04675f18028f5113429496bcda6d
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-11 11:12:45 +0000

    Add Perks and check if exist and attribute responses

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -6,7 +6,48 @@ namespace HeroPerk
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            string arg = args[0];
+
+            Perks myPerks = 0;
+            
+            foreach (char c in arg)
+            {
+                switch (c)
+                {
+                    case 'a':
+                        myPerks ^= Perks.AutoHeal;
+                        break;
+
+                    case 'w':
+                        myPerks ^= Perks.WarpShift;
+                        break;
+                    
+                    case 's':
+                        myPerks ^= Perks.Stealth;
+                        break;
+                    
+                    case 'd':
+                        myPerks ^= Perks.DoubleJump;
+                        break;
+
+                    default:
+                        Console.WriteLine("!Unknown Perk!");
+                        return;
+                }
+                
+            }
+
+            if (myPerks != 0)
+                Console.WriteLine(myPerks);
+            else
+                Console.WriteLine("!No perks at all!");
+
+            if ((myPerks & Perks.DoubleJump) == Perks.DoubleJump && (myPerks & Perks.Stealth) == Perks.Stealth)
+                Console.WriteLine("!Silent jumper!");
+
+            if((myPerks & Perks.AutoHeal) != Perks.AutoHeal)
+                Console.WriteLine("!Not gonna make it!");
+
         }
     }
 }
